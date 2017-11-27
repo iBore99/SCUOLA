3948731d140c9aebf6e4bc3bdafd8724f0c3bc7f
iBore99 <studente.borelli.cristian@gmail.com>
2017-11-27 23:55:12 +0100

    mv

---
+++ Informatica/esBertoliCorsi.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE IF NOT EXISTS accademia;
+
+CREATE TABLE IF NOT EXISTS docenti(
+    id_docente INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    cognome VARCHAR(25),
+    nome VARCHAR(25)
+);
+
+CREATE TABLE IF NOT EXISTS studenti(
+    matricola CHAR(5) PRIMARY KEY NOT NULL,
+    cognome VARCHAR(25),
+    nome VARCHAR(25),
+    data_nascita DATE,
+    indirizzo VARCHAR(25),
+    citta VARCHAR(25),
+    email VARCHAR(30),
+    cellulare VARCHAR(20)
+); 
+
+
+CREATE TABLE IF NOT EXISTS  corsi(
+    cod_corso CHAR(2) PRIMARY KEY NOT NULL,
+    id_docente INT NOT NULL,
+    titolo VARCHAR(25),
+    ore INT,
+    num_esami INT,
+    data_inizio_validita DATE,
+    data_fine_validita DATE,
+
+    FOREIGN KEY (id_docente) REFERENCES docenti(id_docente)
+);
+
+CREATE TABLE IF NOT EXISTS esami(
+    id_esame INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    cod_corso CHAR(2) NOT NULL,
+    matricola CHAR(5) NOT NULL,
+    voto INT,
+    superato CHAR(1),
+    data_esame DATE,
+ 
+
+    FOREIGN KEY (cod_corso) REFERENCES corsi(cod_corso), 
+    FOREIGN KEY (matricola) REFERENCES studenti(matricola)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS iscrizioni(
+    id_iscrizione INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    cod_corso CHAR(2) NOT NULL,
+    matricola CHAR(5) NOT NULL,
+    data_iscrizione DATE,
+    importo DECIMAL(8,2),
+    superato CHAR(1),
+    voto_finale INT,
+
+    
+
+    FOREIGN KEY (cod_corso) REFERENCES corsi(cod_corso), 
+    FOREIGN KEY (matricola) REFERENCES studenti(matricola)
+);
